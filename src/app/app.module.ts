76853910e79c5463ae7a79744e323adae65bcb68
Neha Navgale <nn78d@mail.umkc.edu>
2019-02-27 22:50:24 -0600

    Added Firebase configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,25 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+import {LoginPageModule} from './login/login.module';
+import {RegistrationPageModule} from './registration/registration.module';
+
+const config = {
+  apiKey: 'AIzaSyAB73hk8ugjIX2W3wyqIBhg2NGOIrQ2y3M',
+  authDomain: 'lab-5-9e7fb.firebaseapp.com',
+  databaseURL: 'https://lab-5-9e7fb.firebaseio.com',
+  projectId: 'lab-5-9e7fb',
+  storageBucket: 'lab-5-9e7fb.appspot.com',
+  messagingSenderId: '1014906653653'
+};
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, AngularFireDatabaseModule, AngularFireModule,
+  AngularFireModule.initializeApp(config)],
   providers: [
     StatusBar,
     SplashScreen,
