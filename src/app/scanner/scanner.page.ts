1359d91ef2eb37b899a42c7c6eb564c373d9419f
naveenamadepally13 <47007814+naveenamadepally13@users.noreply.github.com>
2019-03-01 17:21:33 -0600

    saved scanned data to db

--- src/app/scanner/scanner.page.ts
+++ src/app/scanner/scanner.page.ts
@@ -25,6 +25,8 @@ export class ScannerPage {
    this.barcodeScanner.scan().then(barcodeData => {
      console.log('Barcode data', barcodeData);
      this.scaneddataText = barcodeData.text;
+     this.saveData.username = this.userid;
+     this.saveData.scannedData = this.scaneddataText;
        this.angularFireDatabase.object(`/scaneddata/${this.userid}`).set(this.saveData);
    }).catch(err => {
      console.log('Error', err);
